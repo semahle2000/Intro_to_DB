06c1e0d0dfb927bfc73881caeba0bd47af0f8523
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-02-02 00:54:53 +0200

    Add SQL script to insert a single row into Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,9 @@
 -- Select the alx_book_store database
 USE alx_book_store;
 
--- Insert a single row into the Customers table with a different customer_id
+-- Delete the existing row with customer_id = 1
+DELETE FROM Customers WHERE customer_id = 1;
+
+-- Insert a single row into the Customers table
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (2, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
