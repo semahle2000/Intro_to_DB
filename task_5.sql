9b97e247d5f33eb7a99d7e50b7e4aa454292f1fd
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-02-02 16:53:27 +0200

    modified

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,9 @@
 USE alx_book_store;
-SELECT * FROM customers WHERE customer_id = 1;
 
-USE alx_book_store;
 -- Insert a new customer into the customers table
 UPDATE customers
 SET customer_name = 'Cole Baidoo', email = 'cbaidoo@sandtech.com', address = '123 Happiness Ave.'
 WHERE customer_id = 1;
+
+-- Select all rows from the customers table to verify the insertion
+SELECT * FROM customers;
