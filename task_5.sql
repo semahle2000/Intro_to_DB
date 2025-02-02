c0ee6d158f761853fea68cfae98bcea41a46fa75
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-02-02 16:44:15 +0200

    modify script

--- task_5.sql
+++ task_5.sql
@@ -2,7 +2,7 @@ USE alx_book_store;
 SELECT * FROM customers WHERE customer_id = 1;
 
 USE alx_book_store;
-
+-- Insert a new customer into the customers table
 UPDATE customers
 SET customer_name = 'Cole Baidoo', email = 'cbaidoo@sandtech.com', address = '123 Happiness Ave.'
 WHERE customer_id = 1;
