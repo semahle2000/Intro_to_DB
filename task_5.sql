66ca7da005f1277416c26410214a55a7c2a582ec
Asemahle Botha <asemahlebothaasez@gmail.com>
2025-02-02 16:26:30 +0200

    Modified

--- task_5.sql
+++ task_5.sql
@@ -1,9 +1,5 @@
--- Select the alx_book_store database
 USE alx_book_store;
 
--- Delete the existing row with customer_id = 1
-DELETE FROM Customers WHERE customer_id = 1;
-
--- Insert a single row into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+UPDATE customers
+SET customer_name = 'Cole Baidoo', email = 'cbaidoo@sandtech.com', address = '123 Happiness Ave.'
+WHERE customer_id = 1;
